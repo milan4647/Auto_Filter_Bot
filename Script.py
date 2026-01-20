ec91e694ff8e6d85ae82a987dd425c25d01b0063
milan4647 <s58262277@gmail.com>
2026-01-20 20:34:41 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -229,7 +229,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/dreamxbotz">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/dreamxbotz">[ ᴅʀᴇᴀᴍxʙᴏᴛᴢ ]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/MoviesHub_Hindi">{file_name}</a></b>"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
